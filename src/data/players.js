a2ae26fd0db0738823eef517e95ec8d0b54e337a
matibbaez <matiascaneda3@gmail.com>
2025-12-11 20:08:45 -0300

    primer commit

---
+++ src/data/players.js
@@ -0,0 +1,9 @@
+export const futbolistas = [
+    "Lionel Messi", "Cristiano Ronaldo", "Dibu Martínez",
+    "Kylian Mbappé", "Vinicius Jr", "Erling Haaland",
+    "Lamine Yamal", "Julián Álvarez", "Neymar Jr",
+    "Kevin De Bruyne", "Mohamed Salah", "Robert Lewandowski",
+    "Zlatan Ibrahimović", "Luka Modrić", "Harry Maguire",
+    "Darwin Núñez", "Richarlison", "Paulo Dybala",
+    "Kun Agüero", "Ronaldinho"
+];
