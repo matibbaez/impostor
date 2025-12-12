342f83affb26ae7b1440db45742b224f1f995cef
matibbaez <matiascaneda3@gmail.com>
2025-12-12 12:32:59 -0300

    Nueva base de jugadores

--- src/data/players.js
+++ src/data/players.js
@@ -1,9 +1,125 @@
 export const futbolistas = [
-    "Lionel Messi", "Cristiano Ronaldo", "Dibu Martínez",
-    "Kylian Mbappé", "Vinicius Jr", "Erling Haaland",
-    "Lamine Yamal", "Julián Álvarez", "Neymar Jr",
-    "Kevin De Bruyne", "Mohamed Salah", "Robert Lewandowski",
-    "Zlatan Ibrahimović", "Luka Modrić", "Harry Maguire",
-    "Darwin Núñez", "Richarlison", "Paulo Dybala",
-    "Kun Agüero", "Ronaldinho"
+    // --- 🔥 ACTUALIDAD RABIOSA (Los de moda) ---
+    "Lamine Yamal",
+    "Endrick",
+    "Cole Palmer",
+    "Nico Williams",
+    "Jude Bellingham",
+    "Vinícius Jr",
+    "Kylian Mbappé",
+    "Erling Haaland",
+    "Rodrygo",
+    "Pedri",
+    "Gavi",
+    "Florian Wirtz",
+    "Jamal Musiala",
+    "Darwin Núñez",
+    "Luis Díaz",
+    "Alejandro Garnacho",
+    "Phil Foden",
+    "Bukayo Saka",
+    "Rafael Leão",
+    "Khvicha Kvaratskhelia",
+    "Victor Osimhen",
+    "Achraf Hakimi",
+    "Alphonso Davies",
+
+    // --- 🌶️ LA BANDA DEL QUILOMBO (Polémicos / Personajes) ---
+    "Mario Balotelli",    // "Why always me?"
+    "Mauro Icardi",       // El mediático
+    "Zlatan Ibrahimović", // Dios
+    "Luis Suárez",        // El mordisco
+    "Pepe",               // El carnicero
+    "Sergio Ramos",       // El capitán
+    "Neymar Jr",          // Fiestas y caídas
+    "Paul Pogba",         // Talento y líos
+    "Richarlison",        // El pombo
+    "Dibu Martínez",      // El bailecito
+    "Arturo Vidal",       // El King
+    "Gary Medel",         // El Pitbull
+    "Teo Gutiérrez",      // El revólver (Picante local)
+    "Felipe Melo",        // El enemigo
+    "Chilavert",          // El bulldog
+    "Marco Materazzi",    // El cabezazo de Zidane
+
+    // --- 🇦🇷 CAMPEONES DEL MUNDO (Intocables) ---
+    "Lionel Messi",
+    "Ángel Di María",
+    "Julián Álvarez",
+    "Enzo Fernández",
+    "Rodrigo De Paul",
+    "Alexis Mac Allister",
+    "Lautaro Martínez",
+    "Cuti Romero",
+    "Nicolás Otamendi",
+    "Paulo Dybala",
+    "Leandro Paredes",
+    "Lisandro Martínez",
+    "Papu Gómez",         // (Polémico post-mundial)
+
+    // --- 👑 GOATs Y LEYENDAS ---
+    "Cristiano Ronaldo",
+    "Diego Maradona",
+    "Pelé",
+    "Ronaldo Nazário",
+    "Ronaldinho",
+    "Zinedine Zidane",
+    "Johan Cruyff",
+    "Thierry Henry",
+    "Paolo Maldini",
+    "Franz Beckenbauer",
+    "Andrés Iniesta",
+    "Xavi Hernández",
+
+    // --- 🏹 ARQUEROS ---
+    "Gianluigi Buffon",
+    "Iker Casillas",
+    "Manuel Neuer",
+    "Thibaut Courtois",
+    "Alisson Becker",
+    "Marc-André ter Stegen",
+    "Ederson",
+
+    // --- 🏟️ FIGURAS HISTÓRICAS ARGENTINAS ---
+    "Juan Román Riquelme",
+    "Martín Palermo",
+    "Carlos Tevez",
+    "Kun Agüero",
+    "Gabriel Batistuta",
+    "Hernán Crespo",
+    "Javier Zanetti",
+    "Juan Sebastián Verón",
+    "Pablo Aimar",
+    "Ariel Ortega",
+    "Enzo Francescoli",
+    "Mario Kempes",
+    "Fernando Redondo",
+    "Mascherano",
+    "Pipita Higuaín",
+    "Pocho Lavezzi",
+
+    // --- 🌍 CRACKS DE ELITE (Vigentes) ---
+    "Kevin De Bruyne",
+    "Mohamed Salah",
+    "Harry Kane",
+    "Robert Lewandowski",
+    "Luka Modrić",
+    "Toni Kroos",
+    "Antoine Griezmann",
+    "Bernardo Silva",
+    "Rodri",
+    "Virgil van Dijk",
+    "Bruno Fernandes",
+    "Heung-min Son",
+    "Frenkie de Jong",
+    "Marcus Rashford",
+    "Ousmane Dembélé", // El mosquito
+
+    // --- 🧠 TÉCNICOS QUE FUERON JUGADORES ---
+    "Pep Guardiola",
+    "Cholo Simeone",
+    "Lionel Scaloni",
+    "Marcelo Gallardo",
+    "Zinedine Zidane",
+    "Xabi Alonso"
 ];
